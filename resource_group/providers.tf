b25baa568eafa639b597ee7d4a7236645f7b914c
Kiranug <kiran.ug123@gmail.com>
2022-11-24 18:28:32 +0530

    Update providers.tf

--- resource_group/providers.tf
+++ resource_group/providers.tf
@@ -1,29 +1,36 @@
+# Terraform Block
 terraform {
-  required_version = ">=0.12"
-
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" 
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.0"
     }
   }
-      backend "azurerm" {
-        resource_group_name  = "tfstate"
-        storage_account_name = "tfstate30935"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }  
-  
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  
-} 
-  
-  
-  provider "azurerm" {
-  features {}
+# Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name   = "tfstate"
+    storage_account_name  = "tfstate30935"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }   
+}
+
+# Provider Block
+provider "azurerm" {
+ features {}          
+}
+
+# Random String Resource
+resource "random_string" "myrandom" {
+  length = 6
+  upper = false 
+  special = false
+  number = false   
 }
 
 
