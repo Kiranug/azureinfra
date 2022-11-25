6ddd0e2b309f166c3d64a75b5d2b0f861ebdc3f4
Kiranug <kiran.ug123@gmail.com>
2022-11-25 13:49:13 +0530

    Update providers.tf

--- resource_group/providers.tf
+++ resource_group/providers.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
   features {}
 
   subscription_id             = "var.ARM_SUBSCRIPTION_ID"
-  client_id                   = "var.90117df8-8825-455a-82df-3eea5d6ddf36"
+  client_id                   = "var.ARM_CLIENT_ID"
   client_certificate_path     = "var.ARM_CLIENT_CERTIFICATE_PATH"
   client_certificate_password = "var.ARM_CLIENT_CERTIFICATE_PASSWORD"
   tenant_id                   = "var.ARM_TENANT_ID"
