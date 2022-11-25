5d64a21c8f5801760a87461583a9c6f13a951ccd
Kiranug <kiran.ug123@gmail.com>
2022-11-25 15:39:39 +0530

    Update providers.tf

--- resource_group/providers.tf
+++ resource_group/providers.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 
   subscription_id             = "var.subscription_id"
   client_id                   = "var.client_id"
-  client_certificate_path     = "var.client_certificate_path"
+  client_certificate_path     = "/home/azureuser/service-principal1.pfx"
   client_certificate_password = "var.client_certificate_password!"
   tenant_id                   = "var.tenant_id"
 }
