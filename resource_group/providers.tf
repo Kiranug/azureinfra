9d6ea7f60a4ebbc9b1c853841a1748b612107795
Kiranug <kiran.ug123@gmail.com>
2022-11-25 13:32:52 +0530

    Update providers.tf

--- resource_group/providers.tf
+++ resource_group/providers.tf
@@ -1,18 +1,24 @@
-terraform {
-  required_version = ">=0.12"
+variable "client_certificate_path" {}
+variable "client_certificate_password" {}
 
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "=3.0.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id             = "var.ARM_SUBSCRIPTION_ID"
+  client_id                   = "var.90117df8-8825-455a-82df-3eea5d6ddf36"
+  client_certificate_path     = var.client_certificate_path
+  client_certificate_password = var.client_certificate_password
+  tenant_id                   = "var.ARM_TENANT_ID"
 }
