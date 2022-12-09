13e1807ab5617bbecfb4972b6a1677444d3c7afa
Kiranug <kiran.ug123@gmail.com>
2022-12-09 16:17:23 +0530

    Create provider.tf

---
+++ VirtualNetwork/provider.tf
@@ -0,0 +1,24 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  
+  # Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name   = "tfstate"
+    storage_account_name  = "tfstate30935"
+    container_name        = "tfstate"
+    key                   = "dev-terraform.tfstate"
+  } 
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
