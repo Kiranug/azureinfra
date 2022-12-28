dd862c5082b31d2997c26f7beb016e36aa6a0b07
Kiranug <kiran.ug123@gmail.com>
2022-12-28 18:54:48 +0530

    Update resource_group.tf

--- resource_group/resource_group.tf
+++ resource_group/resource_group.tf
@@ -16,5 +16,9 @@ resource "azurerm_resource_group" "rg1" {
   name     = random_pet.rg_name1.id
 }
 
+resource "azurerm_resource_group" "southcenter" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name1.id
+}
 
 
