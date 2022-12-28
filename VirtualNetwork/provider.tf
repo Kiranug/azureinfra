9270ff5e773b7b50416e3b2ab198e65f4c486930
Kiranug <kiran.ug123@gmail.com>
2022-12-28 19:24:08 +0530

    Update provider.tf

--- VirtualNetwork/provider.tf
+++ VirtualNetwork/provider.tf
@@ -14,7 +14,7 @@ terraform {
     resource_group_name   = "tfstate"
     storage_account_name  = "tfstate30935"
     container_name        = "tfstate"
-    key                   = "dev-terraform.tfstate"
+    key                   = "vnet-terraform.tfstate"
   } 
 }
 
