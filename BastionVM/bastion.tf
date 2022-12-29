e096b54f470433cb96fa8a013c9b614944cff02d
Kiranug <kiran.ug123@gmail.com>
2022-12-30 00:17:10 +0530

    Update bastion.tf

--- BastionVM/bastion.tf
+++ BastionVM/bastion.tf
@@ -1,1 +1,41 @@
+resource "azurerm_resource_group" "example" {
+  name     = "bastion-resources"
+  location = "West Europe"
+}
 
+
+resource "azurerm_network_interface" "example" {
+  name                = "bastion-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.bastionsubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F1"
+  admin_username      = "adminuser"
+  admin_password      = "Swami@123"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
