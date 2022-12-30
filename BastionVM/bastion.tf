3231f29c8cfecdb0d195519ac76ed2766508ce66
Kiranug <kiran.ug123@gmail.com>
2022-12-30 13:49:53 +0530

    Update bastion.tf

--- BastionVM/bastion.tf
+++ BastionVM/bastion.tf
@@ -4,20 +4,20 @@ resource "azurerm_resource_group" "example" {
 }
 resource "azurerm_network_interface" "example" {
   name                = "bastion-nic"
-  location            = data.terraform_remote_state.networking.output.Resource_group_location
-  resource_group_name = data.terraform_remote_state.networking.output.Resource_group_name
+  location            = data.terraform_remote_state.networking.outputs.Resource_group_location
+  resource_group_name = data.terraform_remote_state.networking.outputs.Resource_group_name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = data.terraform_remote_state.networking.output.bastion_subnet_id
+    subnet_id                     = data.terraform_remote_state.networking.outputs.bastion_subnet_id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "example" {
   name                = var.bastionvm_name
-  resource_group_name = data.terraform_remote_state.networking.output.Resource_group_name
-  location            = data.terraform_remote_state.networking.output.Resource_group_location
+  resource_group_name = data.terraform_remote_state.networking.outputs.Resource_group_name
+  location            = data.terraform_remote_state.networking.outputs.Resource_group_location
   size                = "Standard_F1"
   admin_username      = "adminuser"
   admin_password      = "Swami@123"
