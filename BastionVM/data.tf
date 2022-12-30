1b59ee90738a784f1c3cedaef9a1dd6e07ec5b9c
Kiranug <kiran.ug123@gmail.com>
2022-12-30 13:51:25 +0530

    Update data.tf

--- BastionVM/data.tf
+++ BastionVM/data.tf
@@ -1,6 +1,7 @@
 data "terraform_remote_state" "networking" {
   backend = "azurerm"
   config = {
+    resource_group_name   = "tfstate"
     storage_account_name = "tfstate30935"
     container_name       = "tfstate"
     key                  = "vnet-terraform.tfstate"
