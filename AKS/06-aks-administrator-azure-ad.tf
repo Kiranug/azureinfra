448160fe152c65a99fd2deb294a323f373e37d55
Kiranug <kiran.ug123@gmail.com>
2023-02-24 17:35:39 +0530

    Update 06-aks-administrator-azure-ad.tf

--- AKS/06-aks-administrator-azure-ad.tf
+++ AKS/06-aks-administrator-azure-ad.tf
@@ -1,7 +1,15 @@
 # Create Azure AD Group in Active Directory for AKS Admins
-resource "azuread_group" "aks_administrators" {
+#resource "azuread_group" "aks_administrators" {
   #name        = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
-  display_name   = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
-  description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-${var.environment} cluster."
+#  display_name   = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
+ # description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-${var.environment} cluster."
+ # security_enabled = true
+#}
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_group" "example" {
+  display_name     = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
+  owners           = [data.azuread_client_config.current.object_id]
   security_enabled = true
 }
