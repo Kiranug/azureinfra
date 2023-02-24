0f7a1a39833c3394000adeb8ffa23c81972a4443
Kiranug <kiran.ug123@gmail.com>
2023-02-24 15:35:20 +0530

    Update variables.tf

--- AKS/variables.tf
+++ AKS/variables.tf
@@ -33,7 +33,7 @@ variable "environment" {
 
 # SSH Public Key for Linux VMs
 variable "ssh_public_key" {
-  default = "/home/azureuser/.ssh/askprodkeys.pub"
+  default = "/opt/askprodkeys.pub"
   description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
 }
 
