2ccb2df129995ab040c6bf2d130c346cefac1482
Kiranug <kiran.ug123@gmail.com>
2023-02-24 13:14:50 +0530

    Update aks-cluster.tf

--- aks_infra/aks-cluster.tf
+++ aks_infra/aks-cluster.tf
@@ -1,4 +1,4 @@
-resource "azurerm_kubernetes_cluster" "default" {
+resource "azurerm_kubernetes_cluster" "aks_rg" {
   name                = "${azurerm_resource_group.aks_rg.name}"
   location            = "${azurerm_resource_group.aks_rg.location}"
   resource_group_name = "${azurerm_resource_group.aks_rg.name}"
