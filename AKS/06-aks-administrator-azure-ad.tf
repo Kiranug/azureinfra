671cd62093302581516c29cfc9aba8755ea2ca0f
Kiranug <kiran.ug123@gmail.com>
2023-02-24 17:28:48 +0530

    Update 06-aks-administrator-azure-ad.tf

--- AKS/06-aks-administrator-azure-ad.tf
+++ AKS/06-aks-administrator-azure-ad.tf
@@ -3,4 +3,5 @@ resource "azuread_group" "aks_administrators" {
   #name        = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
   display_name   = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
   description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-${var.environment} cluster."
+  security_enabled = true
 }
